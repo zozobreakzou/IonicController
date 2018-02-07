496ed3f9da464e4e373d1cb243c361301fb2f384
zozobreak <zozobreakzou@s3graphics.com>
2018-02-07 10:08:14 +0800

    [MobileController] lock controller page landscape orientation.

--- src/pages/controller/controller.ts
+++ src/pages/controller/controller.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import {SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
 
 /**
  * Generated class for the ControllerPage page.
@@ -19,13 +20,22 @@ export class ControllerPage {
   constructor(
       public navCtrl: NavController,
       public navParams: NavParams,
-      private sanitizer: DomSanitizer) {
+      private sanitizer: DomSanitizer,
+      private screenOrientation: ScreenOrientation) {
 
     this.controller_url = this.sanitizer.bypassSecurityTrustResourceUrl(navParams.data.controller_url);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ControllerPage');
+  }
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter ControllerPage');
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+  }
+  ionViewDidLeave() {
+    console.log('ionViewWillLeave ControllerPage');
+    this.screenOrientation.unlock();
   }
 
   private controller_url: SafeResourceUrl;
