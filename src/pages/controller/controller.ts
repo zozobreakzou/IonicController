895fe983945a6f66d28325476ec222e5880f516d
zozobreak <zozobreakzou@s3graphics.com>
2018-03-05 10:46:26 +0800

    [MobileController] relay touch and mouse event from iframe to parent.

--- src/pages/controller/controller.ts
+++ src/pages/controller/controller.ts
@@ -33,12 +33,68 @@ export class ControllerPage {
     console.log('ionViewWillEnter ControllerPage');
     this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
   }
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter ControllerPage');
+    let f = <HTMLIFrameElement>document.getElementById("present_frame");
+    if ( f && f.contentWindow ) {
+      let event_types = [
+        "click", "dblclick", "mousedown", "mouseup", "mousemove",
+        "pointerdown", "pointerup", "pointermove",, "pointercancel",
+        "touchstart", "touchend", "touchmove", "touchcancel"
+      ];
+
+      for (let  e of event_types ) {
+        f.contentWindow.removeEventListener(e, this.relayEventListener);
+      }
+      this.relayEventListener = this.relayEvent.bind(this, document.getElementById("main_content"));
+      for (let  e of event_types ) {
+        f.contentWindow.addEventListener(e, this.relayEventListener);
+      }
+    }
+  }
   ionViewDidLeave() {
     console.log('ionViewWillLeave ControllerPage');
     this.screenOrientation.unlock();
+  }
+
+  relayEvent(target, event) {
+    try {
+      let newEvent = new event.constructor(event.type, event);
+
+      if("view" in newEvent) {
+        Object.defineProperty(newEvent, "view", { writable: true, value: window });
+      }
+
+      ["touches", "targetTouches", "changedTouches"].every( (touches_name, index, array) => {
+        let touches = event[touches_name];
+        if(touches) {
+          let cloneTouchs = [];
+          for ( let touch of touches ) {
+            cloneTouchs.push(document.createTouch(window, target, touch.identifier, touch.pageX, touch.pageY, touch.screenX, touch.screenY));
+          }
+          let newTouchList = document.createTouchList.apply(document, cloneTouchs);
+          Object.defineProperty(newEvent, touches_name, { writable: true, value: newTouchList});
+        }
+        return true;
+      });
+      target.dispatchEvent(newEvent);
+    } catch(e) {
+      console.log(e);
+      let f = <HTMLIFrameElement>document.getElementById("present_frame");
+      if ( f && f.contentWindow ) { 
+        f.contentWindow.removeEventListener(event.type, this.relayEventListener);
+      }
+    }
+  }
+  onTap(e){
+    console.log("tap");
+  }
+  onSwipe(e) {
+    console.log("swipe");
   }
   onExit() {
     this.navCtrl.pop();
   }
   private controller_url: SafeResourceUrl;
+  private relayEventListener;
 }
