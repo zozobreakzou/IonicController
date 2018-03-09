b3b62d15fbed20d561652ca6a6ce20c5d4fd6d8d
zozobreak <zozobreakzou@s3graphics.com>
2018-03-09 11:20:42 +0800

    [MobileController] icon and splash can be generated automatilly.

--- build_android.bat
+++ build_android.bat
@@ -8,11 +8,13 @@ if /i "%1" == "--debug" (
         del release_pkgs\MaxWallController-Debug.apk
     )
     echo starting build debug apk...
+    call ionic cordova resources
     call ionic cordova build android --debug
 )else (
     if exist release_pkgs\MaxWallController.apk (
         del release_pkgs\MaxWallController.apk
     )
     echo starting build release apk...
+    call ionic cordova resources
     call ionic cordova build android --prod --release --buildConfig build\android\build.json
 )
