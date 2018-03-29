215f27a7c2b9d4f5a10ab834818817bcf9ffc469
zozobreakzou <zozobreakzou@viatech.com.cn>
2018-03-29 08:53:56 +0800

    [MobileController] add try-catch for ios input event setup.

--- src/pages/controller/controller.ts
+++ src/pages/controller/controller.ts
@@ -63,21 +63,26 @@ export class ControllerPage {
     console.log("onFrameLoad ControllerPage");
 
     if ( this.controllerURL ) {
-      let f = <HTMLIFrameElement>document.getElementById("present_frame");
-      if ( f && f.contentWindow ) {
-        let event_types = [
-          "click", "dblclick", "mousedown", "mouseup", "mousemove",
-          "pointerdown", "pointerup", "pointermove",, "pointercancel",
-          "touchstart", "touchend", "touchmove", "touchcancel"
-        ];
-
-        for (let  e of event_types ) {
-          f.contentWindow.removeEventListener(e, this.relayEventListener);
+      try {
+        let f = <HTMLIFrameElement>document.getElementById("present_frame");
+        if ( f && f.contentWindow ) {
+          let event_types = [
+            "click", "dblclick", "mousedown", "mouseup", "mousemove",
+            "pointerdown", "pointerup", "pointermove",, "pointercancel",
+            "touchstart", "touchend", "touchmove", "touchcancel"
+          ];
+  
+          for (let  e of event_types ) {
+            f.contentWindow.removeEventListener(e, this.relayEventListener);
+          }
+          this.relayEventListener = this.relayEvent.bind(this, document.body);
+          for (let  e of event_types ) {
+            f.contentWindow.addEventListener(e, this.relayEventListener);
+          }
         }
-        this.relayEventListener = this.relayEvent.bind(this, document.body);
-        for (let  e of event_types ) {
-          f.contentWindow.addEventListener(e, this.relayEventListener);
-        }
+      } catch (error) {
+        console.log("setup iframe input event error.");
+        console.log(error);
       }
 
       if ( this.loading ) {
